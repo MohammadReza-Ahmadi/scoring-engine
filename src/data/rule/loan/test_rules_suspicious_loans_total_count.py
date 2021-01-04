7b7b58e92339031945057211f74da10477ed15a7
mohammad-reza <mohammad-reza@mohammad-reza-ubuntu-20.04>
2021-01-04 12:36:44 +0330

    commit test_rules_suspicious_loans_total_count

---
+++ src/data/rule/loan/test_rules_suspicious_loans_total_count.py
@@ -0,0 +1,19 @@
+from data.done_trades import DoneTrade
+from service.util import get_random_digits_str
+
+
+def test_import_done_trades_data(self):
+    DoneTrade.drop_collection()
+    for i in range(1):
+        dt = DoneTrade()
+        dt.user_id = 1
+        dt.timely_trades_count_of_last_3_months = get_random_digits_str(2)
+        dt.timely_trades_count_between_last_3_to_12_months = get_random_digits_str(2)
+        dt.past_due_trades_count_of_last_3_months = get_random_digits_str(2)
+        dt.past_due_trades_count_between_last_3_to_12_months = get_random_digits_str(2)
+        dt.arrear_trades_count_of_last_3_months = get_random_digits_str(2)
+        dt.arrear_trades_count_between_last_3_to_12_months = get_random_digits_str(2)
+        dt.trades_total_balance = get_random_digits_str(9)
+        dt.total_delay_days = get_random_digits_str(2)
+        gs.save_document(dt)
+        print("flowing document is saved: ", dt.to_json())
